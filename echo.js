50c4829639f1a5018dee29d9c5bb4443e545c8bc
ramborell23 <jerelldavis@ac.c4q.nyc>
2017-11-01 11:35:43 -0400

    Echo exercise

--- echo.js
+++ echo.js
@@ -1,7 +1,7 @@
 var header= document.querySelector('h1')
 document.addEventListener('keypress',function(event){
     if(!event.ctrlKey){
-        header.innerHTML = event.key;
+        header.innerHTML += event.key;
         if (event.key>='A' && event.key<='Z'){
             header.style.textDecoration='underline'
         }else if ('.?!'.includes(event.key)){
