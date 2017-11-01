ba447890553741771dd3efce700f3989da01fbfb
ramborell23 <jerelldavis@ac.c4q.nyc>
2017-11-01 11:29:36 -0400

    Echo exercise

---
+++ echo.js
@@ -0,0 +1,12 @@
+var header= document.querySelector('h1')
+document.addEventListener('keypress',function(event){
+    if(!event.ctrlKey){
+        header.innerHTML = event.key;
+        if (event.key>='A' && event.key<='Z'){
+            header.style.textDecoration='underline'
+        }else if ('.?!'.includes(event.key)){
+            header.style.textDecoration='none'
+            
+        }
+    }
+})
